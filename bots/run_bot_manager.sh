a89f57658689d3ba1a7bd3a3b1c151bb37d20ffc
Omer Ofir <omerofir@Omers-MacBook-Pro-M4.local>
2025-09-07 20:07:24 +0300

    adding sh

--- bots/run_bot_manager.sh
+++ bots/run_bot_manager.sh
@@ -2,13 +2,7 @@
 # or use #!/bin/bash if your default shell is bash
 
 # go to your repo root
-cd "/Users/$(whoami)/PycharmProjects/Omers_stocks" || exit 1
+cd "/Users/$(whoami)/PythonProjects/Omers_stocks" || exit 1
 
-# ensure this folder is on PYTHONPATH so 'bots' imports work
 export PYTHONPATH="$PWD"
-
-# pick the Python you use in VS Code (adjust if needed)
-PYTHON_EXE="$(which python3)"
-
-# run as module from the repo root
-"$PYTHON_EXE" -u -m bots.bot_meneger
+"./.venv/bin/python" -u -m bots.bot_meneger
