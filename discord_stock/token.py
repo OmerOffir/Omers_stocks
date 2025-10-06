2e74971bea9ed9aaedc1252547edfa23aacee224
Omer Ofir <omerofir@Omers-MacBook-Pro-M4.local>
2025-10-06 12:11:49 +0300

    fixing bot

--- discord_stock/token.py
+++ discord_stock/token.py
@@ -1,4 +1,4 @@
 class discord_tok:
-    dis_1= "MTQwNDgxMzM3MjIwOTUwMDIxMA."
-    dis_2=".GTeRXK.J90qO-AB4diIQe6aVEWb"
-    dis_3= "_At9L1a5lJFyAJp5MQ"
+    dis_1= "MTQyNDY4Mzk1NjU2MDk4NjIxMg."
+    dis_2="G-UuUe.nQV4bFmvOifQIpexgN8H"
+    dis_3= "_wyOIXzELk_qUx8dqI"
